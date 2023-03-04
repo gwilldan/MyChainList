b046bbeebd261b32c250e3e39a1f55742c664bdb
gwilldan <sayhi2godswill@gmail.com>
2023-03-04 22:34:25 +0100

    Adding Chain Arrays

---
+++ src/Data/ChainArray.js
@@ -0,0 +1,101 @@
+const ChainArray = [
+    {
+        name: "Ethereum Mainnet",
+        chainID: "1",
+        currency: "ETH",
+        symbol: URL(),
+    },
+
+    {
+        name: "Ubiq Mainnet",
+        chainID: "8",
+        currency: "UBQ",
+        symbol: URL(),
+    },
+
+    {
+        name: "Optimism Mainnet",
+        chainID: "10",
+        currency: "OP",
+        symbol: URL(),
+    },
+
+    {
+        name: "Elastos Mainnet",
+        chainID: "20",
+        currency: "ELT",
+        symbol: URL(),
+    },
+
+    {
+        name: "Kardiachain Mainnet",
+        chainID: "24",
+        currency: "KDC",
+        symbol: URL(),
+    },
+
+    {
+        name: "Cronos Mainnet",
+        chainID: "25",
+        currency: "CRS",
+        symbol: URL(),
+    },
+
+    {
+        name: "RSK",
+        chainID: "30",
+        currency: "RSK",
+        symbol: URL(),
+    },
+
+    {
+        name: "Telos Mainnet",
+        chainID: "40",
+        currency: "TLS",
+        symbol: URL(),
+    },
+
+    {
+        name: "XDC",
+        chainID: "50",
+        currency: "XDC",
+        symbol: URL(),
+    },
+
+    {
+        name: "CSC",
+        chainID: "52",
+        currency: "ETH",
+        symbol: URL(),
+    },
+
+    {
+        name: "ZYK",
+        chainID: "55",
+        currency: "ZYK",
+        symbol: URL(),
+    },
+
+    {
+        name: "Binance",
+        chainID: "56",
+        currency: "BNB",
+        symbol: URL(),
+    },
+
+    {
+        name: "Syscoin",
+        chainID: "57",
+        currency: "SYS",
+        symbol: URL(),
+    },
+
+    {
+        name: "Gochain",
+        chainID: "60",
+        currency: "GOC",
+        symbol: URL(),
+    },
+];
+
+export default ChainArray;
--- src/components/ChainTiles/ChainTiles.js
+++ src/components/ChainTiles/ChainTiles.js
@@ -1,63 +1,60 @@
 import React from 'react';
 import "./ChainTiles.css";
+import {FaEthereum, } from "react-icons/fa"
+import "react-icons/fi"
+import { FiChevronDown } from 'react-icons/fi';
 
 function ChainTiles(props) {
     return (
+        <div className='ch'>
         <div className='ChainTiles'>
-            <div className=''>
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-
-                <p>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
+            <div className='ChainTiles__name'>
+                <FaEthereum className='ChainTiles__name-icon'/>
+                <h3>Ethereum Mainnet</h3>
             </div>
+            <div className='ChainTiles__ChainData'>
+                <p className='ChainTiles__ChainData-ID'>ChainID</p>
+                <p className='ChainTiles__ChainData-Currency'>Currency</p>
+                <p className='ChainTiles__ChainData-IDResult'>1</p>
+                <p className='ChainTiles__ChainData-CurrencyResult'>ETH</p>
+            </div>
+
+            <button>Connect Wallet</button>
+            <FiChevronDown />
+        </div>
+
+        <div className='ChainTiles'>
+            <div className='ChainTiles__name'>
+                <FaEthereum className='ChainTiles__name-icon'/>
+                <h3>Ethereum Mainnet</h3>
+            </div>
+            <div className='ChainTiles__ChainData'>
+                <p className='ChainTiles__ChainData-ID'>ChainID</p>
+                <p className='ChainTiles__ChainData-Currency'>Currency</p>
+                <p className='ChainTiles__ChainData-IDResult'>1</p>
+                <p className='ChainTiles__ChainData-CurrencyResult'>ETH</p>
+            </div>
+
+            <button>Connect Wallet</button>
+            <FiChevronDown />
+        </div>
+
+        <div className='ChainTiles'>
+            <div className='ChainTiles__name'>
+                <FaEthereum className='ChainTiles__name-icon'/>
+                <h3>Ethereum Mainnet</h3>
+            </div>
+            <div className='ChainTiles__ChainData'>
+                <p className='ChainTiles__ChainData-ID'>ChainID</p>
+                <p className='ChainTiles__ChainData-Currency'>Currency</p>
+                <p className='ChainTiles__ChainData-IDResult'>1</p>
+                <p className='ChainTiles__ChainData-CurrencyResult'>ETH</p>
+            </div>
+
+            <button>Connect Wallet</button>
+            <FiChevronDown />
+        </div>
+
         </div>
     );
 }
